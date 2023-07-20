8617639c2539f36cce00d4c46dddc37cc6950310
7pluto <1937218124@qq.com>
2023-07-20 20:14:32 +0800

    增加轮廓的最大邻接矩形，将截取的区域限制在最大邻接矩形内（还是存在区域外的情况） 对代码进行简单封装

--- disposeROIs.py
+++ disposeROIs.py
@@ -1,55 +1,83 @@
 import os
-
 import cv2
 from matplotlib import pyplot as plt
 from datetime import datetime
 import cv2
 import numpy as np
-from sklearn.cluster import MeanShift, DBSCAN, KMeans
+from sklearn.cluster import KMeans
 
 current_time = datetime.now().strftime("%Y%m%d_%H%M%S")
 
 
-def remove_illumination(gray):
-    gray = cv2.convertScaleAbs(gray)
+def Clahe(gray_image):
+    gray_image = cv2.convertScaleAbs(gray_image)
     clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
-    equalized = clahe.apply(gray)
-    return equalized
+    img_clahe = clahe.apply(gray_image)
+    return img_clahe
 
 
-def retinex_multiscale(gray, sigma_list):
-    result = np.zeros_like(gray, dtype=np.float32)
+def retinex_multiscale(gray_image, sigma_list):
+    result = np.zeros_like(gray_image, dtype=np.float32)
     for sigma in sigma_list:
-        blur_image = cv2.GaussianBlur(gray, (0, 0), sigma)
-        reflectance = np.log1p(gray) - np.log1p(blur_image)
+        blur_image = cv2.GaussianBlur(gray_image, (0, 0), sigma)
+        reflectance = np.log1p(gray_image) - np.log1p(blur_image)
         result += reflectance
 
     result /= len(sigma_list)
     result = np.expm1(result)
-    result = (result * 255).astype(np.uint8)
-    return result
+    img_retinex = (result * 255).astype(np.uint8)
+    return img_retinex
 
 
+def extract_filename(image_path):
+    # Split the image path using the directory separator '/'
+    filename = image_path.split(os.path.sep)[-1]
+    return filename
+
+
+def namestr(obj, namespace):
+    return [name for name in namespace if namespace[name] is obj]
+
+
+def plt_show(n, m, plt_list):
+    show_x = 3
+    show_y = int(len(plt_list)/3+1)
+    for i in range(len(plt_list)):
+        image = cv2.cvtColor(plt_list[i], cv2.COLOR_BGR2RGB)
+        plt.suptitle("img_single__ROI_" + str(n) + "_" + str(m), fontsize=15)
+        plt.subplot(show_y, show_x, i+1)
+        plt.imshow(image)
+        plt.title(namestr(plt_list[i], globals())[0])
+        plt.xticks([]), plt.yticks([])
+
+    new_folder_path = "C:\\Users\\chenwh\\PycharmProjects\\dropSeg\\rect_outputs\\plt"
+    os.makedirs(new_folder_path, exist_ok=True)
+    filename = f"{new_folder_path}\\leaf_{n}_{m}.png"
+    print(filename)
+    plt.show()
+
+
+show_list = []
 for i in range(1, 50):
-    for j in range(10):
-        path = "C:\\Users\\chenwh\\PycharmProjects\\dropSeg\\outputs\\" + str(i)
+    for j in range(3):
+        path = "C:\\Users\\chenwh\\PycharmProjects\\dropSeg\\rect_outputs\\" + str(i)
         image_path = path + "\\img_single__ROI_" + str(j) + ".jpg"
         print(image_path)
 
         image = cv2.imread(image_path)
 
-        # gray
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        ret, th = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU)
+        # Gray
+        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        ret, threshold_gray = cv2.threshold(gray_image, 0, 255, cv2.THRESH_OTSU)
 
-        image_equalized = remove_illumination(gray)
-        ret2, th2 = cv2.threshold(image_equalized, 0, 255, cv2.THRESH_OTSU)
+        image_equalized = Clahe(gray_image)
+        ret2, img_clahe = cv2.threshold(image_equalized, 0, 255, cv2.THRESH_OTSU)
 
         image_float = image.astype(np.float32) / 255.0
         sigma_list = [15, 80, 250]
         enhanced_image = retinex_multiscale(image_float, sigma_list)
-        enhanced_image = cv2.cvtColor(enhanced_image, cv2.COLOR_BGR2GRAY)
-        ret3, th3 = cv2.threshold(enhanced_image, 0, 255, cv2.THRESH_OTSU)
+        enhanced_gray_image = cv2.cvtColor(enhanced_image, cv2.COLOR_BGR2GRAY)
+        ret3, img_retinex = cv2.threshold(enhanced_gray_image, 0, 255, cv2.THRESH_OTSU)
 
         # RGB
         B, G, R = cv2.split(image)
@@ -58,59 +86,28 @@ for i in range(1, 50):
         G = G * mask
         B = B * mask
         new_img = cv2.merge([B, G, R])
-        new_gray = cv2.cvtColor(new_img, cv2.COLOR_BGR2GRAY)
-        # _, _, new_gray = cv2.split(image)
-        new_ret, new_th = cv2.threshold(new_gray, 0, 255, cv2.THRESH_OTSU)
+        new_gray_image = cv2.cvtColor(new_img, cv2.COLOR_BGR2GRAY)
+        ret4, threshold_rgb = cv2.threshold(new_gray_image, 0, 255, cv2.THRESH_OTSU)
 
-        image_equalized_2 = remove_illumination(new_gray)
-        ret5, th5 = cv2.threshold(image_equalized_2, 0, 255, cv2.THRESH_OTSU)
+        image_equalized_2 = Clahe(new_gray_image)
+        ret5, img_clahe_rgb = cv2.threshold(image_equalized_2, 0, 255, cv2.THRESH_OTSU)
 
         image_float = new_img.astype(np.float32) / 255.0
         sigma_list = [15, 80, 250]
         enhanced_image_2 = retinex_multiscale(image_float, sigma_list)
-        _, _, enhanced_image_2 = cv2.split(enhanced_image_2)
-        ret6, th6 = cv2.threshold(enhanced_image_2, 0, 255, cv2.THRESH_OTSU)
+        _, _, enhanced_gray_image_2 = cv2.split(enhanced_image_2)
+        ret6, img_retinex_rgb = cv2.threshold(enhanced_gray_image_2, 0, 255, cv2.THRESH_OTSU)
 
-        gray_image = np.float32(new_gray)
+        gray_image_float = np.float32(new_gray_image)
         kmeans_clusters = 2  # K-means聚类数目
         kmeans = KMeans(n_clusters=kmeans_clusters, n_init=10)
-        kmeans.fit(gray_image.reshape(-1, 1))
-        segmented_image_kmeans = kmeans.labels_.reshape(gray_image.shape)
+        kmeans.fit(gray_image_float.reshape(-1, 1))
+        segmented_image_kmeans = kmeans.labels_.reshape(gray_image_float.shape)
         binary_image_kmeans = np.zeros_like(segmented_image_kmeans, dtype=np.uint8)
         binary_image_kmeans[segmented_image_kmeans == np.argmax(np.bincount(segmented_image_kmeans.flatten()))] = 255
 
-        intersection_image = np.logical_and(new_th, th).astype(np.uint8) * 255
+        intersection_image = np.logical_and(threshold_rgb, threshold_gray).astype(np.uint8) * 255
 
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        plt.subplot(241), plt.imshow(image, "gray")
-        plt.title("source"), plt.xticks([]), plt.yticks([])
+        show_list = [image, threshold_gray, threshold_rgb, img_retinex, img_clahe, img_clahe_rgb, img_retinex_rgb]
 
-        plt.subplot(242), plt.imshow(th, "gray")
-        plt.title("gray " + str(ret)), plt.xticks([]), plt.yticks([])
-
-        plt.subplot(243), plt.imshow(new_th, "gray")
-        plt.title("RGB split " + str(new_ret)), plt.xticks([]), plt.yticks([])
-
-        plt.subplot(244), plt.imshow(binary_image_kmeans, "gray")
-        plt.title("k-means"), plt.xticks([]), plt.yticks([])
-
-        plt.subplot(245), plt.imshow(th3, "gray")
-        plt.title("hgray" + str(ret3)), plt.xticks([]), plt.yticks([])
-
-        plt.subplot(246), plt.imshow(th2, "gray")
-        plt.title("qgray" + str(ret2)), plt.xticks([]), plt.yticks([])
-
-        plt.subplot(247), plt.imshow(th6, "gray")
-        plt.title("hRGB" + str(ret6)), plt.xticks([]), plt.yticks([])
-
-        plt.subplot(248), plt.imshow(th5, "gray")
-        plt.title("qRGB" + str(ret5)), plt.xticks([]), plt.yticks([])
-
-        # plt.show()
-
-        new_folder_path = "C:\\Users\\chenwh\\PycharmProjects\\dropSeg\\illumination_outputs\\" + str(i)
-        os.makedirs(new_folder_path, exist_ok=True)
-        filename = f"{new_folder_path}\\leaf_{i}_{j}.png"
-        print(filename)
-        plt.savefig(filename)
-
+        plt_show(i, j, show_list)
--- rangeROIs.py
+++ rangeROIs.py
@@ -1,5 +1,5 @@
 import os
-
+import largestinteriorrectangle as lir
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
@@ -28,65 +28,190 @@ def show_mask(mask, ax, random_color=False):
     ax.imshow(mask_image)
 
 
+def maximum_internal_rectangle_2(img, contours):
+    contour = contours[0].reshape(len(contours[0]), 2)
+    rect = []
+    for i in range(len(contour)):
+        x1, y1 = contour[i]
+        for j in range(len(contour)):
+            x2, y2 = contour[j]
+            area = abs(y2 - y1) * abs(x2 - x1)
+            rect.append(((x1, y1), (x2, y2), area))
+
+    all_rect = sorted(rect, key=lambda x: x[2], reverse=True)
+
+    if all_rect:
+        best_rect_found = False
+        index_rect = 0
+        nb_rect = len(all_rect)
+
+        while not best_rect_found and index_rect < nb_rect:
+
+            rect = all_rect[index_rect]
+            (x1, y1) = rect[0]
+            (x2, y2) = rect[1]
+
+            valid_rect = True
+
+            x = min(x1, x2)
+            while x < max(x1, x2) + 1 and valid_rect:
+                if any(img[y1, x]) == 0 or any(img[y2, x]) == 0:
+                    valid_rect = False
+                x += 1
+
+            y = min(y1, y2)
+            while y < max(y1, y2) + 1 and valid_rect:
+                if any(img[y, x1]) == 0 or any(img[y, x2]) == 0:
+                    valid_rect = False
+                y += 1
+
+            if valid_rect:
+                best_rect_found = True
+
+            index_rect += 1
+
+        if best_rect_found:
+            # 如果要在灰度图img_gray上画矩形，请用黑色画（0,0,0）
+            cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 0), 3)
+            # 在mask上画最小内接矩形
+            # cv2.imshow("rec", img)
+            return img, x1, y1, x2, y2
+            # cv2.waitKey(0)
+
+        else:
+            print("No rectangle fitting into the area")
+
+    else:
+        print("No rectangle found")
+
+
+def random_crop_inner_rectangle(img, x1, y1, x2, y2, crop_width, crop_height):
+    # 确保x1 < x2 和 y1 < y2
+    x1, x2 = min(x1, x2), max(x1, x2)
+    y1, y2 = min(y1, y2), max(y1, y2)
+
+    # 计算裁剪的范围，考虑crop_width和crop_height的一半
+    max_x = x2 - crop_width // 2
+    min_x = x1 + crop_width // 2
+    max_y = y2 - crop_height // 2
+    min_y = y1 + crop_height // 2
+
+    # 创建一个空白图像作为默认返回值
+    cropped_img = np.zeros((crop_height, crop_width), np.uint8)
+
+    # 检查裁剪范围是否有效，有效则随机裁剪
+    if min_x < max_x and min_y < max_y:
+        crop_x = np.random.randint(min_x, max_x)
+        crop_y = np.random.randint(min_y, max_y)
+
+        # 裁剪图像
+        cropped_img = img[crop_y:crop_y+crop_height, crop_x:crop_x+crop_width]
+
+    return cropped_img
+
+
 model_type = "vit_t"
 sam_checkpoint = "C:\\Users\\chenwh\\Downloads\\MobileSAM-master\\weights\\mobile_sam.pt"
 device = "cuda"
 image_name = "img_single_"
+mobile_sam = sam_model_registry[model_type](checkpoint=sam_checkpoint)
+mobile_sam.to(device=device)
+mobile_sam.eval()
+predictor = SamPredictor(mobile_sam)
+# 图像文件名
+# img_single_1.jpg
 
 for i in range(1, 50):
     image_path = "E:/data_images/" + image_name + str(i) + ".jpg"
     image = cv2.imread(image_path)
 
-
-    mobile_sam = sam_model_registry[model_type](checkpoint=sam_checkpoint)
-    mobile_sam.to(device=device)
-    mobile_sam.eval()
-    predictor = SamPredictor(mobile_sam)
     predictor.set_image(image)
-    input_point = np.array([[1800, 1700]])
+    input_point = np.array([[1600, 1800]])
     input_label = np.array([1])
     masks, scores, logits = predictor.predict(input_point,
         point_labels=input_label,
         multimask_output=False,)
 
-
-    # for i, (mask, score) in enumerate(zip(masks, scores)):
-    #     plt.figure(figsize=(10,10))
-    #     plt.imshow(image)
-    #     show_mask(masks[i], plt.gca())
-    #     show_points(input_point, input_label, plt.gca())
-    #     plt.title(f"Mask {i}, Score: {score:.3f}", fontsize=18)
-    #     plt.axis('off')
-    #     plt.show()
-
-    new_folder_path = "outputs/" + str(i)
-    os.makedirs(new_folder_path, exist_ok=True)
-
-    mask = masks[0].astype(np.uint8)
-    mask = ~masks[0]
-    mask = mask + 255
+    mask = ~masks[0] + 255
     mask = np.repeat(mask[:, :, np.newaxis], 3, axis=2).astype(np.uint8)
     res = cv2.bitwise_and(image, mask)
 
+    # 为加速计算，将image进行resize后再求最小内接矩形
+    img = mask
+    scale_percent = 9
+    width = int(img.shape[1] * scale_percent / 100)
+    height = int(img.shape[0] * scale_percent / 100)
+    dim = (width, height)
+    resized = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
+
+    # 求最小内接矩形
+    img_gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
+    ret, img_bin = cv2.threshold(img_gray, 0, 255, cv2.THRESH_BINARY)
+    contours, _ = cv2.findContours(img_bin, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
+    src, x1, y1, x2, y2 = maximum_internal_rectangle_2(resized, contours)
+
+    # 将矩形放大回原来的大小
+    x1 = int(x1 / scale_percent * 100)
+    x2 = int(x2 / scale_percent * 100)
+    y1 = int(y1 / scale_percent * 100)
+    y2 = int(y2 / scale_percent * 100)
+
+    # 查看提示点与叶片的位置关系
+    # for k, (mask, score) in enumerate(zip(masks, scores)):
+    #     plt.figure(figsize=(10,10))
+    #     plt.suptitle(image_name + str(i), fontsize=20)
+    #     plt.imshow(image)
+    #     show_mask(masks[k], plt.gca())
+    #     show_points(input_point, input_label, plt.gca())
+    #     plt.title(f"Mask {k}, Score: {score:.3f}", fontsize=18)
+    #     plt.axis('off')
+    #
+    #     plt_name = "plt/" + image_name + str(i) + "_" + str(k) +".png"
+    #     plt.savefig(plt_name)
+    #     plt.show()
+
+    # 在原图和mask上画最小内接矩形
+    image_rect = image.copy()
+    cv2.rectangle(image_rect, (x1, y1), (x2, y2), (255, 0, 0), 20)
+    # cv2.namedWindow("image_rect", cv2.WINDOW_NORMAL)
+    # cv2.imshow("image_rect", image_rect)
+    # cv2.imshow("src", src)
+    # cv2.waitKey(0)
+
+    new_folder_path = "rect_outputs/" + str(i)
+    os.makedirs(new_folder_path, exist_ok=True)
+
+    # 保存叶片黑色背景图
     filename2 = f"{new_folder_path}/{image_name}_leaf.jpg"
     cv2.imwrite(filename2, res)
-
+    # 保存原图
     filename3 = f"{new_folder_path}/{image_name}_image.jpg"
     cv2.imwrite(filename3, image)
+    # 原图和mask上画最小内接矩形
+    filename4 = f"{new_folder_path}/{image_name}_image_rect.jpg"
+    cv2.imwrite(filename4, image_rect)
+    filename5 = f"{new_folder_path}/{image_name}_mask_rect.jpg"
+    cv2.imwrite(filename5, src)
 
-    for j in range(10):
-        indices = np.where(masks[0] != 0)
-        random_index = np.random.choice(len(indices[0]))
-        random_x, random_y = indices[1][random_index], indices[0][random_index]
-        rect_width, rect_height = 300, 300
-        rect_x = max(random_x - rect_width // 2, 0)
-        rect_y = max(random_y - rect_height // 2, 0)
-        random_rectangle = image[rect_y:rect_y + rect_height, rect_x:rect_x + rect_width]
+    for j in range(3):
+        crop_width = (x2 - x1) * 0.3
+        crop_height = (y2 - y1) * 0.3
 
+        random_rectangle = random_crop_inner_rectangle(image, x1, y1, x2, y2, 300, 300)
+
+        # random_rectangle = cv2.cvtColor(random_rectangle, cv2.COLOR_BGR2RGB)
+        plt.subplot(1, 3, j + 1)
+        plt.imshow(random_rectangle)
+        plt.xticks([]), plt.yticks([])
+
+        # cv2.imshow("random_rectangle", random_rectangle)
+        # cv2.waitKey(0)
 
         filename = f"{new_folder_path}/{image_name}_ROI_{j}.jpg"
         cv2.imwrite(filename, random_rectangle)
-
+    #
+    # plt.show()
+    # cv2.waitKey(0)
 
     print("已处理： " + image_path)
 
